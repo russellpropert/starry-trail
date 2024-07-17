42ce57f54d965df39dadc18415ac417b17f1c7c8
Russell Propert <russellpropert@gmail.com>
2024-07-17 11:15:54 -0400

    Removing touchstart and touchend event handlers.

--- starry_trail.js
+++ starry_trail.js
@@ -18,14 +18,6 @@ window.document.onmousemove = (event) => {
   yCoordinate = event.pageY;
 }
 
-document.addEventListener('touchstart', (event) => {
-  event.preventDefault();
-});
-
-document.addEventListener('touchend', (event) => {
-  event.preventDefault();
-});
-
 document.addEventListener('touchmove', (event) => {
   xCoordinate = event.pageX;
   yCoordinate = event.pageY;
